ccf8b40267ecb42d40960bb86a5a5cb6e1312cc1
ElcimarSilva <elcimar8@gmail.com>
2022-03-10 23:19:09 -0300

    atualizaçao projeto

--- PyGsheet.py
+++ PyGsheet.py
@@ -1,16 +1,17 @@
 from __future__ import print_function
 import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '10QWyaTRxVWMz5oOxzA2dkfooHHh0MHV9G3Cf_xfipXU' # id da planilha
-SAMPLE_RANGE_NAME = 'Página1!A:B' # intervalo de celulas da planilha
+SAMPLE_SPREADSHEET_ID = '1bizMrFKIhDFlkblAn92-XYgO3vrlpake7MbyR2RLdg8'
+SAMPLE_RANGE_NAME = 'Página1!A:B'
 
 
 def main():
@@ -24,35 +25,43 @@ def main():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'client_secret.json', SCOPES)
+                'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
-    service = build('sheets', 'v4', credentials=creds)
+    try:
+        service = build('sheets', 'v4', credentials=creds)
 
-    # Ler a planilha
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                range=SAMPLE_RANGE_NAME).execute()
-    values = result.get('values', [])
-    if not values:
-        print('No data found.')
-    # else:
-    #     for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            #print('%s, %s' % (row[0], row[4]))
-    print(values)
+        sheet = service.spreadsheets()
 
-    #adicionar ou atualizar
-    valores_adicionar = [
-        ["Cima", "001"],
-        ["Nat", "002"],
-    ]
-    result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+        def chama_planilha():
+            result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                        range=SAMPLE_RANGE_NAME).execute()
+            values = result.get('values', [])
+
+            if not values:
+                print('No data found.')
+            return values
+
+        print(chama_planilha())
+
+        
+        valores_adicionar = [
+            ["Cima", "001"],
+            ["Nat", "002"],
+        ]
+
+        result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
                                 range='Página1!A2', valueInputOption='USER_ENTERED',
                                    body={'values': valores_adicionar}).execute()
+        
+        print(chama_planilha())
+
+    except HttpError as err:
+        print(err)
+
 
 if __name__ == '__main__':
     main()
--- readme.md
+++ readme.md
@@ -1,9 +1,27 @@
-Video
+# Video explicativo
 https://www.youtube.com/watch?v=ZU30e4gkV8g
-25:14
 
+
+# Instruções execução do projeto
+Criar ambiente virtual
+instalar biblioteca
+configurar e executar projeto
+
+# Biblioteca necessaria
 pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 
+# Links uteis
 https://developers.google.com/sheets/api/quickstart/python
 https://console.cloud.google.com/apis/credentials?hl=pt-br&project=integracaogsheet-333922
-https://docs.google.com/spreadsheets/d/10QWyaTRxVWMz5oOxzA2dkfooHHh0MHV9G3Cf_xfipXU/edit#gid=0
+
+
+# Instruções basicas do projeto
+1-Passo criar projeto no google cloud
+2-Passo criar app na Tela de permissão OAuth
+3-Passo criar credencial OAuth
+
+1-Passo Pegar código exemplo na documentação
+2-Alterar ID da planilha
+3-Alterar Range da planilha
+4-Baixar credenciais do google cloud e colocar no codigo.
+5-Retirar escopo read only para poder editar
